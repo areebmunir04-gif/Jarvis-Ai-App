ad81129fd8ca0b1bddb6a0c6f215ba44ce7eb16c
Areeb <areebmunir04@gmail.com>
2026-05-14 11:15:33 +0700

    fixed client component

---
+++ frontend/app/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [message, setMessage] = useState("");
+  const [reply, setReply] = useState("");
+
+  const sendMessage = async () => {
+    try {
+      const res = await axios.post(
+        "https://jarvis-ai-app-olit.onrender.com/chat",
+        {
+          message,
+        }
+      );
+
+      setReply(res.data.reply);
+    } catch (error) {
+      console.log(error);
+      setReply("Connection failed");
+    }
+  };
+
+  return (
+    <main>
+      <input
+        value={message}
+        onChange={(e) =>
+          setMessage(e.target.value)
+        }
+      />
+
+      <button onClick={sendMessage}>
+        Send
+      </button>
+
+      <div>{reply}</div>
+    </main>
+  );
+}
