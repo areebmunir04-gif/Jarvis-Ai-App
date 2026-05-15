d5c984a440b625c718cfdeab4498a65d65d45de5
Areeb <areebmunir04@gmail.com>
2026-05-15 14:04:21 +0700

    final online backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
-
 require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-const { exec } = require("child_process");
 const Groq = require("groq-sdk");
 
 const app = express();
@@ -16,92 +14,26 @@ app.use(
     allowedHeaders: ["Content-Type"],
   })
 );
+
 app.use(express.json());
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
 });
 
+// TEST ROUTE
 app.get("/", (req, res) => {
+
   res.send("Jarvis backend running");
+
 });
 
+// CHAT ROUTE
 app.post("/chat", async (req, res) => {
 
   try {
 
     const { message } = req.body;
-
-    const lowerMessage =
-      message.toLowerCase();
-
-    // OPEN YOUTUBE
-    if (
-      lowerMessage.includes(
-        "open youtube"
-      )
-    ) {
-
-      exec(
-        'start https://youtube.com'
-      );
-
-      return res.json({
-        reply:
-          "Opening YouTube",
-      });
-    }
-
-    // OPEN GOOGLE
-    if (
-      lowerMessage.includes(
-        "open google"
-      )
-    ) {
-
-      exec(
-        'start https://google.com'
-      );
-
-      return res.json({
-        reply:
-          "Opening Google",
-      });
-    }
-
-    // OPEN CHROME
-    if (
-      lowerMessage.includes(
-        "open chrome"
-      )
-    ) {
-
-      exec(
-        'start chrome'
-      );
-
-      return res.json({
-        reply:
-          "Opening Chrome",
-      });
-    }
-
-    // OPEN NOTEPAD
-    if (
-      lowerMessage.includes(
-        "open notepad"
-      )
-    ) {
-
-      exec(
-        'start notepad'
-      );
-
-      return res.json({
-        reply:
-          "Opening Notepad",
-      });
-    }
 
     let webData = "";
 
@@ -132,7 +64,7 @@ app.post("/chat", async (req, res) => {
     } catch (err) {
 
       console.log(
-        "Search failed"
+        "Tavily search failed"
       );
     }
 
@@ -146,7 +78,7 @@ app.post("/chat", async (req, res) => {
             role: "system",
 
             content:
-              "You are Jarvis AI assistant. Give short fast replies.",
+              "You are Jarvis AI assistant. Give short smart replies.",
           },
 
           {
@@ -183,12 +115,14 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
+// PORT
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT, () => {
 
   console.log(
-    "Server running on port 5000"
+    `Server running on ${PORT}`
   );
+
 });
-
-
-
